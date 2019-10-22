46ef8a11a9c55ce06e98ba91beb41813bcd60c8e
v.reshetnikov <skemobob@gmail.com>
2019-10-22 17:48:40 +0300

    modified signatures of operations

--- include/math_vector.h
+++ include/math_vector.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <stdexcept>
+#include <cassert>
 namespace va_temps
 {
 template <typename T,int size=2>
@@ -10,52 +11,43 @@ public:
     {
         vect=(data.size()==size)?std::vector(data):std::vector(size,0);
     }
-    void Add(Vector const &v)
+    auto Add(Vector const &v) const
     {
-        if(v.vect.size()==vect.size())
+        assert(v.vect.size() == vect.size());
+        Vector tmp=*this;
+        for(size_t i=0;i<vect.size();++i)
         {
-            for(size_t i=0;i<vect.size();++i)
-            {
-                vect.at(i)+=v.vect.at(i);
-            }
+            tmp.vect.at(i)+=v.vect.at(i);
         }
-        else
-        {
-            throw std::invalid_argument("Dims are not the same");
-        }
+        return tmp;
     }
-    void Substract(Vector const &v)
+    auto Substract(Vector const &v) const
     {
-        if(v.vect.size()==vect.size())
+        assert(v.vect.size() == vect.size());
+        Vector tmp=*this;
+        for(size_t i=0;i<vect.size();++i)
         {
-            for(size_t i=0;i<vect.size();++i)
-            {
-                vect.at(i)-=v.vect.at(i);
-            }
+            tmp.vect.at(i)-=v.vect.at(i);
         }
-        else
-        {
-            throw std::invalid_argument("Dims are not the same");
-        }
+        return tmp;
     }
-    auto ScalarMultiply(Vector const &v)
+
+    auto ScalarProduct(Vector const &v) const
     {
-        if(v.vect.size()==vect.size())
-        {
+        assert(v.vect.size() == vect.size());
         double product = 0;
         for (size_t i = 0; i < v.vect.size(); i++)
-                product += (v.vect[i])*(vect[i]);
+            product += (v.vect[i])*(vect[i]);
         return product;
-        }
-        throw std::invalid_argument("Dims are not the same");
     }
-    auto VectorMultiply(Vector const &v)
+
+    auto VectorProduct(Vector const &v) const
     {
+        assert(v.vect.size() == vect.size());
         Vector tmp=*this;
         for (size_t i = 0; i < vect.size(); i++)
             tmp.vect.at(i)= vect[i+1]*v.vect[i+2]-vect[i+2]*v.vect[i+1];
         return tmp;
-
     }
     std::vector<T> getVect() const
     {
