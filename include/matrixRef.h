0a3aa505cf954a604a6ed3a7d46f4323d416859e
v.reshetnikov <skemobob@gmail.com>
2019-10-29 18:47:40 +0300

    Added matrix specialization

--- include/matrixRef.h
+++ include/matrixRef.h
@@ -11,20 +11,20 @@ class Vector;
 
 
 template <typename T>
-class Matrix
+class Matrix<T*>
 {
 public:
-    Matrix():rows_(0),cols_(0),data_(std::vector<T>()){}
-    Matrix(std::size_t rows,std::size_t cols):rows_(rows),cols_(cols),data_(std::vector<T>()){}
-    Matrix(T* pointer,std::size_t rows,std::size_t cols):rows_(rows),cols_(cols),data_(std::vector<T>(pointer,pointer+rows*cols)){}
-    Matrix(std::initializer_list<T> data,std::size_t rows,std::size_t cols):rows_(rows),cols_(cols),data_(std::vector<T>(data)) {}
+    Matrix():rows_(0),cols_(0),data_(std::vector<T*>()){}
+    Matrix(std::size_t rows,std::size_t cols):rows_(rows),cols_(cols),data_(std::vector<T*>()){}
+    Matrix(T* pointer,std::size_t rows,std::size_t cols):rows_(rows),cols_(cols),data_(std::vector<T*>(pointer,pointer+rows*cols)){}
+    Matrix(std::initializer_list<T*> data,std::size_t rows,std::size_t cols):rows_(rows),cols_(cols),data_(std::vector<T*>(data)) {}
     void Print() const
     {
         for(size_t row=0;row<rows_;++row)
         {
             for(size_t col=0;col<cols_;++col)
             {
-                std::cout<<data_.at(col+cols_*row)<<" ";
+                std::cout<<*data_.at(col+cols_*row)<<" ";
             }
             std::cout<<endl;
         }
@@ -34,7 +34,16 @@ public:
         Matrix tmp(*this);
         for(size_t i=0;i<rows_*cols_;++i)
         {
-            tmp.data_.at(i)+=m.data_.at(i);
+            *tmp.data_.at(i)+=*m.data_.at(i);
+        }
+        return tmp;
+    }
+    auto Add(Matrix<T> const & m) const
+    {
+        Matrix tmp(*this);
+        for(size_t i=0;i<rows_*cols_;++i)
+        {
+            *tmp.data_.at(i)+=m.data_.at(i);
         }
         return tmp;
     }
@@ -44,24 +53,49 @@ public:
         Matrix tmp(*this);
         for(size_t i=0;i<rows_*cols_;++i)
         {
-            tmp.data_.at(i)-=m.data_.at(i);
+            *tmp.data_.at(i)-=*m.data_.at(i);
         }
         return tmp;
     }
+    auto Substract(Matrix<T> const & m) const
+    {
+        Matrix tmp(*this);
+        for(size_t i=0;i<rows_*cols_;++i)
+        {
+            *tmp.data_.at(i)-=m.data_.at(i);
+        }
+        return tmp;
+    }
+
     auto Multiply(Matrix const & m) const
     {
-        assert(this->cols_ == m.getRows());
+        assert(this->cols_ == m.getRows() && data_.size()==m.data_.size());
         Matrix tmp(*this);
-        tmp.data_.clear();
-        tmp.data_.resize(cols_*m.getRows(),0);
         for(size_t row=0;row<rows_;++row)
         {
             for(size_t col=0;col<m.getCols();++col)
             {
                 T sum(0);
                 for(size_t row_=0;row_<m.getRows();++row_)
-                    sum+=data_.at(row*cols_+row_)*m.getData().at(row_*m.getCols()+col);
-                tmp.data_.at(row*m.getCols()+col) = sum;
+                    sum+=*data_.at(row*cols_+row_)*(*m.getData().at(row_*m.getCols()+col));
+                *tmp.data_.at(row*m.getCols()+col) = sum;
+            }
+        }
+        tmp.cols_=m.getRows();
+        return tmp;
+    }
+    auto Multiply(Matrix<T> const & m) const
+    {
+        assert(this->cols_ == m.getRows() && data_.size()==m.data_.size());
+        Matrix tmp(*this);
+        for(size_t row=0;row<rows_;++row)
+        {
+            for(size_t col=0;col<m.getCols();++col)
+            {
+                T sum(0);
+                for(size_t row_=0;row_<m.getRows();++row_)
+                    sum+=*data_.at(row*cols_+row_)*m.getData().at(row_*m.getCols()+col);
+                *tmp.data_.at(row*m.getCols()+col) = sum;
             }
         }
         tmp.cols_=m.getRows();
@@ -74,7 +108,7 @@ public:
         {
             for(size_t col=0;col<cols_;++col)
             {
-                tmp.data_.at(col+cols_*row) =value*data_.at(col+cols_*row);
+                *tmp.data_.at(col+cols_*row) =value*(*data_.at(col+cols_*row));
             }
         }
         return tmp;
@@ -89,7 +123,7 @@ public:
         {
             for(size_t col=0;col<cols_;++col)
             {
-                tmp_vector.at(row)+=data_.at(col+cols_*row)*vect.getVect().at(col);
+                tmp_vector.at(row)+=*data_.at(col+cols_*row)*vect.getVect().at(col);
             }
         }
         tmp.setVect(tmp_vector);
@@ -97,13 +131,12 @@ public:
     }
     auto Divide(T const & value) const
     {
-        Matrix<T> tmp(rows_,cols_);
-        tmp.data_.resize(rows_*cols_);
+        Matrix tmp(*this);
         for(size_t row=0;row<rows_;++row)
         {
             for(size_t col=0;col<cols_;++col)
             {
-                tmp.data_.at(col+cols_*row) =data_.at(col+cols_*row)/value;
+                *tmp.data_.at(col+cols_*row) =(*data_.at(col+cols_*row))/value;
             }
         }
         return tmp;
@@ -139,26 +172,7 @@ public:
         }
         return tmp;
     }
-    auto getSubMatrixRef(size_t const & row_start,size_t const & row_end,
-                      size_t const & col_start,size_t const& col_end)
-    {
-        assert(row_end<rows_ && col_end<cols_ && row_start>=0 && col_start>=0);
-        size_t tmp_rows=row_end-row_start+1,tmp_cols =col_end-col_start+1;
-        Matrix<T*> tmp(tmp_rows,tmp_cols);
-        tmp.data_.resize(tmp_rows*tmp_cols);
-        int index=0;
-        for(size_t row=row_start;row<=row_end;++row)
-        {
-            for(size_t col=col_start;col<=col_end;++col)
-            {
-                typename std::vector<T>::iterator it = data_.begin();
-                it+=(col+row*cols_);
-                tmp.data_[index]=&(*it);
-                ++index;
-            }
-        }
-        return tmp;
-    }
+
     std::size_t getRows() const
     {
         return rows_;
@@ -169,7 +183,7 @@ public:
         return cols_;
     }
 
-    std::vector<T> getData() const
+    std::vector<T*> getData() const
     {
         return data_;
     }
@@ -178,15 +192,10 @@ public:
     {
         data_ = data;
     }
-    std::vector<T> data_;
+    std::vector<T*> data_;
 private:
     std::size_t rows_;
     std::size_t cols_;
 
 };
-
-
-
-
-
 }
